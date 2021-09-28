c256defbbd6a317eb7b696417075fab4688bce74
alvinlamSL <alvin_lam@email.com>
2021-09-28 23:56:51 +0800

    Added smaller map for easier testing

--- python/drawMap.py
+++ python/drawMap.py
@@ -4,8 +4,8 @@ class Map:
   def __init__(
     self,
     jsonFile = None,
-    cols = 50,
-    rows = 50,
+    cols = 30,
+    rows = 20,
   ):
     self.cols = cols
     self.rows = rows
@@ -18,84 +18,39 @@ class Map:
         self.rows = jsonData['rows']
         self.tiles = jsonData['tiles']
 
+  def addTracks(self, colStart, colEnd, rowStart, rowEnd, trackName):
+    if colStart == colEnd: colEnd += 1
+    if rowStart == rowEnd: rowEnd += 1
+    for x in range(colStart, colEnd):
+      for y in range(rowStart, rowEnd):
+        self.tiles.append({
+          "col": x,
+          "row": y,
+          "type": "tracks",
+          "name": trackName
+        })
+
   def saveJson(self):
     jTup = {
       "cols": self.cols,
       "rows": self.rows,
       "tiles": self.tiles
     }
-    with open('./map.json', 'w') as jFile:
+    with open('./map3.json', 'w') as jFile:
       json.dump(jTup, jFile)
 
-map = Map(jsonFile='map.json')
-map.tiles.append({
-  "col": 19,
-  "row": 10,
-  "type": "tracks",
-  "name": "esw"
-})
-map.tiles.append({
-  "col": 40,
-  "row": 10,
-  "type": "tracks",
-  "name": "wse"
-})
-map.tiles.append({
-  "col": 18,
-  "row": 11,
-  "type": "tracks",
-  "name": "sne"
-})
-map.tiles.append({
-  "col": 41,
-  "row": 11,
-  "type": "tracks",
-  "name": "snw"
-})
-for x in range(12, 17):
-  map.tiles.append({
-    "col": 18,
-    "row": x,
-    "type": "tracks",
-    "name": "ns"
-  })
-  map.tiles.append({
-    "col": 41,
-    "row": x,
-    "type": "tracks",
-    "name": "ns"
-  })
-map.tiles.append({
-  "col": 18,
-  "row": 17,
-  "type": "tracks",
-  "name": "nse"
-})
-map.tiles.append({
-  "col": 19,
-  "row": 18,
-  "type": "tracks",
-  "name": "enw"
-})
-map.tiles.append({
-  "col": 41,
-  "row": 17,
-  "type": "tracks",
-  "name": "nsw"
-})
-map.tiles.append({
-  "col": 40,
-  "row": 18,
-  "type": "tracks",
-  "name": "wne"
-})
-for x in range(20, 40):
-  map.tiles.append({
-    "col": x,
-    "row": 18,
-    "type": "tracks",
-    "name": "ew"
-  })
-
+map = Map()
+map.addTracks(5, 26, 3, 3, "ew")
+map.addTracks(5, 26, 17, 17, "ew")
+map.addTracks(3, 3, 5, 16, "ns")
+map.addTracks(27, 27, 5, 16, "ns")
+map.addTracks(4, 4, 3, 3, "esw")
+map.addTracks(3, 3, 4, 4, "sne")
+map.addTracks(4, 4, 17, 17, "enw")
+map.addTracks(3, 3, 16, 16, "nse")
+map.addTracks(26, 26, 3, 3, "wse")
+map.addTracks(27, 27, 4, 4, "snw")
+map.addTracks(26, 26, 17, 17, "wne")
+map.addTracks(27, 27, 16, 16, "nsw")
 
 map.saveJson()
--- src/gameLogic/initialiseGameMap.js
+++ src/gameLogic/initialiseGameMap.js
@@ -1,6 +1,6 @@
 // This function fetches the game map from the public folder
 const initialiseGameMap = async () => {
-  const dataFetch = fetch('data/maps/map2.json').then((res) => (
+  const dataFetch = fetch('data/maps/map3.json').then((res) => (
     res.json()
   ));
   const data = await dataFetch;
